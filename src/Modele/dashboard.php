55da9e2e5b32fb01e38aca37a9c167a596023942
Godefroy Lambert <45532493+Ludmino@users.noreply.github.com>
2022-06-21 09:18:28 +0200

    Update dashboard.php

--- src/Modele/dashboard.php
+++ src/Modele/dashboard.php
@@ -34,6 +34,20 @@ include_once "../php/translate.php";
         include '../php/navbar.php';
     ?>
 
+    <?php
+        $ch = curl_init();
+        curl_setopt(
+        $ch,
+        CURLOPT_URL,
+        "http://projets-tomcat.isep.fr:8080/appService?ACTION=GETLOG&TEAM=1337");
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        echo "Raw Data:<br />";
+        echo("$data");
+    ?>
+    
     <div class="boite">
     <div id="RC" class="tabcontent">
         <div class="Canvas">

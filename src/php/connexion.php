4cb322dc087ed0845742e62d1a28040494bed3e0
Godefroy Lambert <45532493+Ludmino@users.noreply.github.com>
2022-06-22 15:31:54 +0200

    Update connexion.php

--- src/php/connexion.php
+++ src/php/connexion.php
@@ -1,8 +1,8 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "app";
+    $servername = "herogu.garageisep.com";
+    $username = "lSexcXMy03_app_g3d";
+    $password = "uMWyer4Yak9fClUk";
+    $dbname = "b8F6e3Zn63_app_g3d";
     // Create connection
     global $conn;
     $conn = new mysqli($servername, $username, $password, $dbname);
